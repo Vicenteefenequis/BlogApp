25a1dd5f7c40f871da9b0fd3517540bd3d7c9810
Vicente Junior <vicente19981@live.com>
2021-12-21 21:29:46 -0300

    Initial Commit

---
+++ Blog/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Blog
+//
+//  Created by Vicente on 21/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
