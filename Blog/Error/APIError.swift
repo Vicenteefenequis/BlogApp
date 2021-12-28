eb123ddb14ecf932b9a690eebc65907a261aa557
Vicente Junior <vicente19981@live.com>
2021-12-28 12:37:56 -0300

    feat: create enums of api error callback

---
+++ Blog/Error/APIError.swift
@@ -0,0 +1,30 @@
+//
+//  APIError.swift
+//  Blog
+//
+//  Created by Vicente on 28/12/21.
+//
+
+import Foundation
+
+
+
+enum APIError: Error {
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from the servuce"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown:
+            return "The error is unknown"
+        }
+    }
+}
