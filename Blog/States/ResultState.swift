4ed83f9c7be2b64741712810039ce6f53aa2ece1
Vicente Junior <vicente19981@live.com>
2021-12-28 12:38:58 -0300

    feat: create enum state of results

---
+++ Blog/States/ResultState.swift
@@ -0,0 +1,17 @@
+//
+//  ResultState.swift
+//  Blog
+//
+//  Created by Vicente on 27/12/21.
+//
+
+import Foundation
+
+
+enum ResultState {
+    case loading
+    case success(content: [Post])
+    case failed(error: Error)
+}
+
+
