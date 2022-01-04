bf9b12e3e91990b623c40a9955561294a2c8b453
Vicente Junior <vicente19981@live.com>
2022-01-04 16:59:03 -0300

    refactor: change message post Description

--- Blog/Model/BlogResponse.swift
+++ Blog/Model/BlogResponse.swift
@@ -31,6 +31,7 @@ struct Blog: Codable, Identifiable {
 
 extension Blog {
     static var dummyData: Blog {
-        .init(idPost: 1, title: "Disney, Gina Carano Firing denounced as Hollywoord Black List, against conservatives - Fox News", postDescription: "Conservative actress Gina Carano found rare support among liberals in the mainstream media following", image: "https://s3-file-uploader-bucket-prod.s3.sa-east-1.amazonaws.com/images/2021-12-16T18%3A12%3A32.119Z.jpeg")
+        .init(idPost: 1, title: "Disney, Gina Carano Firing denounced as Hollywoord Black List, against conservatives - Fox News", postDescription: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+              image: "https://s3-file-uploader-bucket-prod.s3.sa-east-1.amazonaws.com/images/2021-12-16T18%3A12%3A32.119Z.jpeg")
     }
 }
